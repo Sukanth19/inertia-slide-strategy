78388b9d08924d6137d7a513bbbb3602174055c7
Sukanth19 <sukan3066@gmail.com>
2025-12-25 23:30:34 +0530

    feat: implement core game engine and inertia-based state management

--- inertia.py
+++ inertia.py
@@ -92,3 +92,108 @@ MAPS = {
         "stops": [(0, 8), (1, 0), (5, 4), (8, 0), (9, 1), (9, 8), (4, 5), (6, 8), (2, 4), (7, 5)]
     }
 }
+
+
+# ==============================
+# Core Game Engine & State
+# ==============================
+
+class InertiaGame:
+    def __init__(self, map_name):
+        self.map_name = map_name
+        self.reset()
+
+    def reset(self):
+        map_data = MAPS[self.map_name]
+
+        self.rows = map_data["rows"]
+        self.cols = map_data["cols"]
+        self.start_pos = map_data["start"]
+
+        self.board = [[EMPTY for _ in range(self.cols)] for _ in range(self.rows)]
+
+        for r, c in map_data["gems"]:
+            self.board[r][c] = GEM
+        for r, c in map_data["mines"]:
+            self.board[r][c] = MINE
+        for r, c in map_data["stops"]:
+            self.board[r][c] = STOP
+
+        self.ball_pos = self.start_pos
+        self.human_score = 0
+        self.cpu_score = 0
+        self.human_moves = 0
+        self.cpu_moves = 0
+        self.game_over = False
+        self.human_eliminated = False
+        self.cpu_eliminated = False
+        self.total_gems = len(map_data["gems"])
+
+    def change_map(self, map_name):
+        self.map_name = map_name
+        self.reset()
+
+    def simulate_move(self, direction):
+        dr, dc = direction
+        r, c = self.ball_pos
+
+        gems = 0
+        hit_mine = False
+        path = [(r, c)]
+
+        while True:
+            nr, nc = r + dr, c + dc
+
+            if not (0 <= nr < self.rows and 0 <= nc < self.cols):
+                break
+
+            r, c = nr, nc
+            path.append((r, c))
+
+            if self.board[r][c] == GEM:
+                gems += 1
+            elif self.board[r][c] == MINE:
+                hit_mine = True
+                break
+            elif self.board[r][c] == STOP:
+                break
+
+        return (r, c), gems, hit_mine, path
+
+    def make_move(self, direction, is_human=True):
+        if self.game_over:
+            return False, 0, [], False
+
+        end_pos, gems, hit_mine, path = self.simulate_move(direction)
+
+        if hit_mine:
+            self.game_over = True
+            if is_human:
+                self.human_eliminated = True
+            else:
+                self.cpu_eliminated = True
+            return False, 0, path, True
+
+        if end_pos == self.ball_pos:
+            return False, 0, [], False
+
+        self.ball_pos = end_pos
+
+        if is_human:
+            self.human_moves += 1
+        else:
+            self.cpu_moves += 1
+
+        for r, c in path[1:]:
+            if self.board[r][c] == GEM:
+                self.board[r][c] = EMPTY
+                if is_human:
+                    self.human_score += 1
+                else:
+                    self.cpu_score += 1
+
+        if self.human_score + self.cpu_score >= self.total_gems:
+            self.game_over = True
+
+        return True, gems, path, False
+
